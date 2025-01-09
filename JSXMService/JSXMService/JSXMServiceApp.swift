72fb00b019cd7d982534908f120fa22f5b11061c
Waran28 <n.raguwaranggmail.com>
2025-01-09 21:56:51 +0530

    initall commit

---
+++ JSXMService/JSXMService/JSXMServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSXMServiceApp.swift
+//  JSXMService
+//
+//  Created by New on 2025-01-08.
+//
+
+import SwiftUI
+
+@main
+struct JSXMServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
